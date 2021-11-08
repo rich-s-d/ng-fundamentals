467c488c4d03110511e813d31d9b4129d8265cd8
Shane Rich <srichnz@gmail.com>
2021-11-08 22:31:39 +0100

    Finished module 6, routing.

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,10 +1,15 @@
-import { EventsListComponent } from "./events/events-list.component";
-import { EventDetailsComponent } from "./events/event-details/event-details.component";
+import {
+    EventDetailsComponent,
+    EventRouteActivator,
+    EventListResolver,
+    EventsListComponent,
+    CreateEventComponent
+
+    } from "./events/index";
+
 import { Routes } from "@angular/router";
-import { CreateEventComponent } from "./events/create-event.component";
 import { Error404Component } from "./errors/404component";
-import { EventRouteActivator } from "./events/event-details/event-route-activator.service";
-import { EventListResolver } from "./events/events-list-resolver.service";
+
 
 export const appRoutes:Routes = [
     {
@@ -32,7 +37,12 @@ export const appRoutes:Routes = [
     {
         path: '404',
         component: Error404Component,
-    }
+    },
+    {
+        path: 'user',
+        loadChildren: () => import('./user/user.module')
+            .then(m => m.UserModule)
+    },
 
     // {
     //     path: 'contact',
