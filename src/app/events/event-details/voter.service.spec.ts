f7cdbad55aec7943f872d87f5303387dabb187b3
Shane Rich <srichnz@gmail.com>
2021-11-26 21:01:34 +0100

    Last section of  module 15 on unit testing.

--- src/app/events/event-details/voter.service.spec.ts
+++ src/app/events/event-details/voter.service.spec.ts
@@ -21,7 +21,26 @@ describe('VoterService', () => {
             //Assert
             expect(session.voters.length).toBe(1);
             expect(session.voters[0]).toBe('john');
-        }
-
-    )})
+        })
+        it('should call http.delete with the right URL', () => {
+            //Arrange
+            let session = { id: 6, voters: ['joe', 'john']};
+            mockHttp.delete.and.returnValue(of(false));
+            //Act
+            voterService.deleteVoter(3, <ISession>session, 'joe');
+            //Assert
+            expect(mockHttp.delete).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/joe');
+        })
+    })
+    describe('addVoter', () => {
+        it('should call http.post with the right URL', () => {
+            //Arrange
+            let session = { id: 6, voters: ['joe']};
+            mockHttp.post.and.returnValue(of(false));
+            //Act
+            voterService.addVoter(3, <ISession>session, 'joe');
+            //Assert
+            expect(mockHttp.post).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/joe', {}, jasmine.any(Object));
+        })
+    })
 })
